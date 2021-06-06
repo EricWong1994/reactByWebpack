d2c7cce4a3a34052ada68f0eddbb45aed0b696d4
v_wangshihao01 <v_wangshihao01@baidu.com>
2021-06-06 21:11:16 +0800

    copy插件 devServer配置

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path')
 const HtmlWebpackPlugin  = require('html-webpack-plugin')
 const {CleanWebpackPlugin}  = require('clean-webpack-plugin')
 const RemoveCommentsPlugin = require('./remove-comments-plugins')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 // ./webpack.config.js
 /** @type {import('webpack').Configuration} */
 module.exports = {
@@ -35,11 +37,36 @@ module.exports = {
       // }
       template: './src/index.html'
     }),
-    new RemoveCommentsPlugin()
+    new RemoveCommentsPlugin(),
+    // new CopyWebpackPlugin({
+    //   // patterns: 'public' // 需要拷贝的目录或者路径通配符
+    //   // patterns: ['public'] // 需要拷贝的目录或者路径通配符
+    //   patterns: [{
+    //     // from: './aaa/*.md',
+    //     from: path.join(__dirname, 'aaa*.md'),
+    //     to: path.join(__dirname, 'dist')
+        
+    //     // to: './dist/'
+    //   }] // 需要拷贝的目录或者路径通配符
+    // })
+
+    // new CopyWebpackPlugin([
+    //   // 'public'
+    //   'aaa'
+    // ])
+
     // 用于生成 about.html
     // new HtmlWebpackPlugin({
     //   filename: 'about.html'
     // })
-  ]
+  ],
+  devServer: {
+    // contentBase: path.join(__dirname, 'dist'),
+    // contentBase: path.join(__dirname, './aaa'),
+    compress: true,
+    port: 9000,
+    // host: 'dev.lemon.baidu.com',
+    open: true
+  }
   // loader:
 }
