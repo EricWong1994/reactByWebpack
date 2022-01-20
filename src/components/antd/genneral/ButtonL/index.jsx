0c9828c929cac61eda71d22cb7a6652c414ff3d0
v_wangshihao01 <rang1994@icloud.com>
2022-01-20 19:20:09 +0800

    antd 版本升级为4.18.4

--- src/components/antd/genneral/ButtonL/index.jsx
+++ src/components/antd/genneral/ButtonL/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Button} from 'antd'
+// import Button from '/Users/v_wangshihao01/Desktop/github/ant-design/lib/button/button.js';
 import ReactDOM from 'react-dom';
 // const mountNode = document.querySelector('#antd');
 const mountNode = document.querySelector('#app');
@@ -7,7 +8,7 @@ const mountNode = document.querySelector('#app');
 export default function ButtonL() {
 	return (
 		<div>
-			ButtonL
+			ButtonL 222
 			<Button />
 		</div>
 	);
