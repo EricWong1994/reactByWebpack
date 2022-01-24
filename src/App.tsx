9d65ae0e33b975d24687d49d1252a73092876cfa
EricWong1994 <rang1994@icloud.com>
2022-01-24 16:16:12 +0800

    antd button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { ConfigProvider, Button } from 'antd';
 
@@ -11,14 +11,24 @@ export default function App() {
 
 		return suffixCls ? `hhh-${suffixCls}` : 'hhh';
 	};
+	const rxTwoCNChar = /^[\u4e00-\u9fa5]{2}$/;
+	const isTwoCNChar = rxTwoCNChar.test.bind(rxTwoCNChar);
+	const testRes = isTwoCNChar('中文');
+	console.log('testRes: ', testRes);
+	const buttonRefCN = useRef(null);
+	console.log('buttonRefCN: ', buttonRefCN);
 
 	return (
 		<div>
 			<div>App</div>
 			<Button>Button</Button>
 			<br></br>
-			<ConfigProvider componentSize='large' prefixCls={'ericWong'}>
+			<ConfigProvider
+				componentSize='large'
+				// prefixCls={'ericWong'}
+			>
 				<Button>large Button</Button>
+				<Button ref={buttonRefCN}>中文</Button>
 			</ConfigProvider>
 		</div>
 	);
